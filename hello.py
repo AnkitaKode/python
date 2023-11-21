82bbbdede953ce1600dca6c5ca65a7063261db58
Ankita Kumari <ankita00iv@gmail.com>
2023-11-21 21:50:05 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("hello world!")
+name = input("what is your name? ")
+color=input("what is your favourite color?")
+print(name + " likes "+color)
---
+++ if_else.py
@@ -0,0 +1,10 @@
+age=40
+ 
+if age>=18:
+    print("You can drive.")
+elif age>3 and age<18:
+    print("You are in school")
+else:
+    print("You are a child")
+
+print("Thank You")
