2a41a627a571e268bb681e2cca8317a69ab9c0a3
Ankita Kumari <ankita00iv@gmail.com>
2024-02-20 22:45:32 +0530

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 first=input("Enter the first number: ")
 operator=input("Enter the operator (+,-,*,/):")
-second=input("Enter the first number: ")
+second=input("Enter the second number: ")
 
 first=int(first)
 second=int(second)
