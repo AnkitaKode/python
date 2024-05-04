950831a96ac35f3d9fa2b02f4b5b94b978391993
Ankita Kumari <ankita00iv@gmail.com>
2024-05-04 23:36:44 +0530

    initial commit

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+marks={"english":96,"maths":56}
+information={"ram":"dasrath"}
+
+print(marks["english"])
+marks["physics"]=96
+print(marks)
+
+marks["physics"]=99
+print(marks)
---
+++ tuple.py
@@ -0,0 +1,5 @@
+#list[] #tuple() #set{}
+marks=(23,45,28,78,23,65,23,84)
+
+print(marks.count(23))
+print(marks.index(45))
