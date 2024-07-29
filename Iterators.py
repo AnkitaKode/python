569471674b920fa88277d5ad3dc3a580398554fa
Ankita Kumari <ankita00iv@gmail.com>
2024-07-29 23:39:22 +0530

    initial commit

---
+++ Iterators.py
@@ -0,0 +1,23 @@
+for i in range(12):  
+    
+    print( i + 4, end = " ")  
+        
+     
+    if i == 9:  
+        break  
+       
+print()  
+        
+ 
+i = 0 
+while i < 15:  
+        
+   
+    if i == 9:  
+        i += 3  
+        continue  
+    else:  
+         
+        print( i + 2, end = " ")  
+            
+    i += 1  
