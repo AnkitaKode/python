d85519fada4c354b78eae167a263927c6a5367d8
Ankita Kumari <ankita00iv@gmail.com>
2024-08-21 22:48:05 +0530

    initial commit

---
+++ Libraries.py
@@ -0,0 +1,5 @@
+#   NUMPY: Multi-Dimensional Array
+#   PANDA: Provides 2-D spreadsheet e.g., Rows and columns
+#   MatPlotLib: Provides 2-D Graphs and Plots
+#   SciKit-Learn: Provides common algorithms like decision trace
+
