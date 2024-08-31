ff97286d88c35148ffb9b160f4703fe355cd502a
Ankita Kumari <ankita00iv@gmail.com>
2024-08-31 23:35:37 +0530

    initial commit

---
+++ Array.py
@@ -0,0 +1,13 @@
+import array as arr    
+
+a = arr.array('i', [66, 40, 15, 69])   
+
+print("First element is:", a[0])    
+print("Second element is:", a[1])   
+print("Third element is:", a[2])  
+print("Forth element is:", a[3])  
+print("last element is:", a[-1])    
+print("Second last element is:", a[-2])    
+print("Third last element is:", a[-3])    
+print("Forth last element is:", a[-4])    
+print(a[0], a[1], a[2], a[3], a[-1],a[-2],a[-3],a[-4]) 
