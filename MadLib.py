1e15df2f8769fc35eed3014d6a7a2f071d606195
Ankita Kumari <ankita00iv@gmail.com>
2024-09-01 23:59:34 +0530

    initial commit

---
+++ MadLib.py
@@ -0,0 +1,9 @@
+your_name=input("Enter your name: ")
+place=input("Enter where you lives: ")
+age=input("Enter your age: ")
+
+
+print("I am "+ your_name)
+print("I am "+age+" old.")
+print("Lives at "+ place)
+print("Thanks!")
