3fee6dbd7a3ce0c4a080c675dfefda63308a482f
Ankita Kumari <ankita00iv@gmail.com>
2024-09-19 23:56:02 +0530

    initial commit

---
+++ ModuleImp.py
@@ -0,0 +1,5 @@
+import math as mt 
+pi=3.14
+print( "The value of euler's number is", mt.e )
+print( "Calculating tangent of an angle: ", mt.tan(pi/6) ) 
+print( "Calculating square root: ", mt.sqrt(25) )   
