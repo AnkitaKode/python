de88fe44aa60915cb0eb5ab5108bfb7d28a1872b
Ankita Kumari <ankita00iv@gmail.com>
2024-09-23 23:16:39 +0530

    initial commit

---
+++ GCD.py
@@ -0,0 +1,10 @@
+def find_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+gcd = find_gcd(num1, num2)
+print(f"The GCD of {num1} and {num2} is {gcd}")
