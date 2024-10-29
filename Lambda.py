0d760b99f3c78fe723b7ec23af89dfd90c56892e
Ankita Kumari <ankita00iv@gmail.com>
2024-10-29 23:58:48 +0530

    initial commit

---
+++ Lambda.py
@@ -0,0 +1,13 @@
+def reciprocal( num ):  
+    return 1 / num  
+   
+lambda_reciprocal = lambda num: 1 / num  
+   
+# to defined by def keyword  
+print( "Def keyword: ", reciprocal(6) )  
+   
+# to defined by lambda keyword  
+print( "Lambda keyword: ", lambda_reciprocal(6) )  
+
+
+  
