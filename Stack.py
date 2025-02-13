5f00494653017ec4288826dca6be918a191f9978
Ankita Kumari <ankita00iv@gmail.com>
2025-02-13 23:13:33 +0530

    initial commit

---
+++ Stack.py
@@ -0,0 +1,18 @@
+# initial empty stack  
+my_stack = []  
+  
+ 
+my_stack.append('x')  
+my_stack.append('y')  
+my_stack.append('z')  
+  
+  
+print(my_stack)  
+
+print('\nElements poped from my_stack:')  
+print(my_stack.pop())  
+print(my_stack.pop())  
+print(my_stack.pop())  
+  
+print('\nmy_stack after elements are poped:')  
+print(my_stack)
